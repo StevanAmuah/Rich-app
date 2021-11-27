84c369836cde945735de05341ac4b5e9fa34e298
Stevan Amuah <stevancojo89@gmail.com>
2021-11-27 18:58:50 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rich_app
+
+This is one the simple, basic and static mobile application I made using dart with flutter.
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'I rich app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("I am rich"),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        body: const Center(
+          child: Image(
+            height: 200,
+            image: AssetImage('assets/images/diamond.png'),)
+        )
+      )
+    );
+  }
+}
+
